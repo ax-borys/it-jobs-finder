aa9c119da5f8cc048ddbb911db30f75d0ff1dea8
Alex Borysiuk <alexborysiuk@pm.me>
2026-04-22 21:01:39 +0200

    Initial commit

---
+++ apps/parsers/justjoinit-parser/index.ts
@@ -0,0 +1,12 @@
+const url =
+  "https://justjoin.it/api/candidate-api/offers?from=0&itemsCount=100&categories=javascript&cityRadius=30&currency=pln&orderBy=descending&sortBy=publishedAt&keywordType=any&isPromoted=true";
+
+async function parseJustJoinIt() {
+  const response = await fetch(url);
+  const json = await response.json();
+  const jobs = json.data;
+
+  return jobs;
+}
+
+export { parseJustJoinIt };
