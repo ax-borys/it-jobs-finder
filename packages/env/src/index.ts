64ab738ffbff0c4cb5eced412611ac631daa1509
Alex Borysiuk <alexborysiuk@pm.me>
2026-05-01 10:34:08 +0200

    fix: define own __dirname since the path to .env is undefined during drizzle-kit push

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,7 +1,9 @@
 import { config } from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-config({ path: path.resolve(import.meta.dirname, '../../../.env') });
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+config({ path: path.resolve(__dirname, '../../../.env') });
 
 // TODO: add type-checking
 export const env = {
